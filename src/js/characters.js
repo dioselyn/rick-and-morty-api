810d87f01bc172b45a96bd5ff27e7edd260cb1db
Dioselyn Colina <dioze09@gmail.com>
2022-05-11 20:16:17 -0500

    Update loader

--- src/js/characters.js
+++ src/js/characters.js
@@ -1,36 +1,39 @@
-const API = 'https://rickandmortyapi.com/api/character/';
+const API = "https://rickandmortyapi.com/api/character/";
 
-$main = document.querySelector("main"), //var type DOM
-$links = document.querySelector(".links"); //var type DOM
+($main = document.querySelector(".cards-content")), //var type DOM
+  ($links = document.querySelector(".links")); //var type DOM
 
 async function loadCharacter(url) {
-    try {
-        $main.innerHTML = `<img class="loader" src="../assets/img/loader.svg" alt="Loading...">`;
+  try {
+    $main.innerHTML = `<div class="loader-container"><img class="loader" src="../../assets/img/loader.svg" alt="Loading..."></div>`;
 
-        let response = await fetch(url),
-        json = await response.json(),
-        $template = "",
-        $prevLink,
-        $nextLink;
+    let response = await fetch(url),
+      json = await response.json(),
+      $template = "",
+      $prevLink,
+      $nextLink;
 
-        //console.log(json);
+    //console.log(json);
 
-        if(!response.ok) throw { status:response.status,statusText:response.statusText } //catch error
+    if (!response.ok)
+      throw { status: response.status, statusText: response.statusText }; //catch error
 
-        for(let i=0; i < json.results.length; i++) {
-            //console.log(json.results[i]);
-            try {
-                let response = await fetch(json.results[i].url),
-                character = await response.json();
-                //console.log(response, character);
+    for (let i = 0; i < json.results.length; i++) {
+      //console.log(json.results[i]);
+      try {
+        let response = await fetch(json.results[i].url),
+          character = await response.json();
+        //console.log(response, character);
 
-                if(!response.ok) throw { status:response.status,statusText:response.statusText } //catch error
+        if (!response.ok)
+          throw { status: response.status, statusText: response.statusText }; //catch error
 
-            
-                $template += `
+        $template += `
                 <article class="card">
                 <div class="card-image">
-                <img class="img-fluid" src="${character.image}" alt="${character.name}">
+                <img class="img-fluid" src="${character.image}" alt="${
+          character.name
+        }">
                 <span class="badge-top">${character.origin.name}</span></div>
                 
                 <div class="card-body">
@@ -38,42 +41,39 @@ async function loadCharacter(url) {
                 </div>
                 <div class="card-footer">
                 <span class="specie">${character.species}</span>
-                <span class="${character.status.toLowerCase()}">${character.status}</span>
+                <span class="${character.status.toLowerCase()}">${
+          character.status
+        }</span>
                 </div>
                 </article>
                 `;
-            }
-            catch (err) {
-                console.log(err);
-                let message = err.statusText || "An error occurred";
-               $template += `
+      } catch (err) {
+        console.log(err);
+        let message = err.statusText || "An error occurred";
+        $template += `
                
                <p>Error ${err.status}: ${message}</p>
         
-                `; 
-            }
-           }
-      $main.innerHTML = $template; 
-      //console.log(json.info.next);
-      $prevLink = json.info.prev ? `<a href="${json.info.prev}">Previous ⏪</a>`:"";
-      $nextLink = json.info.next ? `<a href="${json.info.next}">Next ⏩</a>`:"";
-      $links.innerHTML = $prevLink + " " + $nextLink;
-
-    } catch (err) {
-        console.log(err);
-        let message = err.statusText || "Character does not exist";
-        $main.innerHTML = `
+                `;
+      }
+    }
+    $main.innerHTML = $template;
+    //console.log(json.info.next);
+    $prevLink = json.info.prev
+      ? `<a href="${json.info.prev}">Previous ⏪</a>`
+      : "";
+    $nextLink = json.info.next ? `<a href="${json.info.next}">Next ⏩</a>` : "";
+    $links.innerHTML = $prevLink + " " + $nextLink;
+  } catch (err) {
+    console.log(err);
+    let message = err.statusText || "Character does not exist";
+    $main.innerHTML = `
         
         <span class="error">Error ${err.status}: ${message}</span>
        
-        `; 
-    }
+        `;
+  }
 }
 
 //Load API
-document.addEventListener("DOMContentLoaded", event => loadCharacter(API));
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", (event) => loadCharacter(API));
